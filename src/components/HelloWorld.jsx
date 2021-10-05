d3aa9c77359148ff13460340b853b1a0e86ddecc
Andre Santos <andresantosas2558487@gmail.com>
2021-10-05 16:02:00 -0300

    first commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function HelloWorld () {
+    return (
+        <>
+        <h2>React é melhor que vuejs</h2>
+        <ul>
+            <li>Home</li>
+            <li>Show</li>
+            <li>About</li>
+        </ul>
+        </>
+    )
+}
+
+export default HelloWorld;
